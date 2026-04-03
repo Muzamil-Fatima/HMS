e044801b50c432746fc26a8a5fc9cab98cb552cc
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2026-04-03 23:43:27 +0500

    Add Appointment

--- 1-HMS/client/src/pages/Appointment.jsx
+++ 1-HMS/client/src/pages/Appointment.jsx
@@ -63,20 +63,139 @@ const Appointment = () => {
             ? false
             : true;
 
-            if(isSlotAvailable){
-              // Add slot to array
-              timeSlots.push({datetime:new Date(currentDate)})
-            }
+        if (isSlotAvailable) {
+          // Add slot to array
+          timeSlots.push({
+            datetime: new Date(currentDate),
+            time: formattedTime,
+          });
+        }
+        // Increment time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
       }
+      setDocSlots((prev) => [...prev, timeSlots]);
     }
   };
-  return (
-    <>
-      <div>
-        <p></p>
+  const bookAppointment = async () => {
+    if (!token) {
+      toast.error("Login to book appointment");
+      return navigate("/login");
+    }
+    const date = docSlots[slotIndex][0].datetime;
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    const slotDate = day + "_" + month + "_" + year;
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/user/book-appointment",
+        { docId, slotDate, slotTime },
+        { headers: { token } },
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getDoctorData();
+        navigate("/my-appointments");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (doctors.length > 0) {
+      fetchDocInfo();
+    }
+  }, [doctors, docId]);
+  useEffect(() => {
+    if (docInfo) {
+      getAvailableSlots();
+    }
+  }, [docInfo]);
+  return docInfo ? (
+    <div>
+      {/* Doctor Details  */}
+      <div className="flex flex-col sm:flex-row gap-4">
+        <div>
+          <img
+            src={docInfo.image}
+            alt=""
+            className="bg-primary w-full sm:max-w-72 rounded-lg "
+          />
+        </div>
+        <div className="flex-1 border border-[#ADADAD] rounded-lg p-8 py-7 bg-white  mx-2 sm:mx-0 -mt-20 sm:mt-0 ">
+          {/* ----- Doc Info : name, degree, experience ----- */}
+          <p className="flex items-center gap-2 text-3xl font-medium text-gray-700">
+            {docInfo.name}{" "}
+            <img src={assets.verified_icon} className="w-5" alt="" />
+          </p>
+          <div className="flex items-center gap-2 mt-1 text-gray-600">
+            <p>
+              {docInfo.degree} - {docInfo.speciality}
+            </p>
+            <button className="py-0.5 px-2 border text-xs rounded-full">
+              {docInfo.experience}
+            </button>
+          </div>
+          {/*  doc about */}
+          <div>
+            <p className="flex items-center gap-1 text-sm font-medium text-[#262626] mt-3">
+              About <img src={assets.info_icon} alt="" className="w-3" />
+            </p>
+            <p className="text-sm text-gray-6000 max-w-175 mt-1">
+              {docInfo.about}
+            </p>
+          </div>
+          <p className="text-gray-600 font-medium mt-4">
+            Appointment fee:{" "}
+            <span className="text-gray-800">
+              {currencySymbol}
+              {docInfo.fees}
+            </span>
+          </p>
+        </div>
       </div>
-    </>
-  );
+      {/* Booking SLots */}
+      <div className="sm:ml-72 sm:pl-4 mt-8 font-medium text-[#565656]">
+        <p>Booking slots</p>
+        <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+          {docSlots.length &&
+            docSlots.map((item, index) => (
+              <div
+                onClick={() => setSlotIndex(index)}
+                key={index}
+                className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? "bg-primary text-white " : "border border-[#DDDDDD]"}`}
+              >
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))}
+        </div>
+        <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
+          {docSlots.length &&
+            docSlots[slotIndex].map((item, index) => (
+              <p
+                onClick={() => setSLotTime(item.time)}
+                key={index}
+                className={`text-sm font-light shrink-0 px-5 py-2 rounded-full cursor-pointer`}
+              ></p>
+            ))}
+        </div>
+        <button
+          onClick={bookAppointment}
+          className="bg-primary text-white text-sm font-light  px-20 py-3 rounded-full my-6"
+        >
+          Book an appointment
+        </button>
+      </div>
+      {/* listing Related doctors */}
+      <RelatedDoctors speciality={docInfo.speciality} docId={docId} />
+    </div>
+  ) : null;
 };
 
 export default Appointment;
